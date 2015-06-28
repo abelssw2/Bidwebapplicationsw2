201fbdaaca44b4c677791084755203bf0de38abb
abel <abelzodiac@gmail.com>
2015-06-28 14:08:08 +0200

    imp update

---
+++ implementationV2.0/GuessBid/src/main/java/it/polimi/guessbid/Controler/PaymentFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.polimi.guessbid.Controler;
+
+import it.polimi.guessbid.Entity.Payment;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author abel
+ */
+@Stateless
+public class PaymentFacade extends AbstractFacade<Payment> {
+    @PersistenceContext(unitName = "com.mycompany_GuessBid_war_1.0-SNAPSHOTPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public PaymentFacade() {
+        super(Payment.class);
+    }
+    
+}
---
+++ implementationV2.0/GuessBid/src/main/java/it/polimi/guessbid/Entity/Payment.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.polimi.guessbid.Entity;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author abel
+ */
+@Entity
+@Table(name = "payment", catalog = "guessbid", schema = "public")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Payment.findAll", query = "SELECT p FROM Payment p"),
+    @NamedQuery(name = "Payment.findById", query = "SELECT p FROM Payment p WHERE p.id = :id"),
+    @NamedQuery(name = "Payment.findByAmount", query = "SELECT p FROM Payment p WHERE p.amount = :amount")})
+public class Payment implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Column(name = "amount", precision = 17, scale = 17)
+    private Double amount;
+    @JoinColumn(name = "auction_id", referencedColumnName = "id")
+    @ManyToOne
+    private Auction auctionId;
+    @JoinColumn(name = "buyer_id", referencedColumnName = "id")
+    @ManyToOne
+    private Buyer buyerId;
+
+    public Payment() {
+    }
+
+    public Payment(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public Auction getAuctionId() {
+        return auctionId;
+    }
+
+    public void setAuctionId(Auction auctionId) {
+        this.auctionId = auctionId;
+    }
+
+    public Buyer getBuyerId() {
+        return buyerId;
+    }
+
+    public void setBuyerId(Buyer buyerId) {
+        this.buyerId = buyerId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Payment)) {
+            return false;
+        }
+        Payment other = (Payment) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "it.polimi.guessbid.Entity.Payment[ id=" + id + " ]";
+    }
+    
+}
